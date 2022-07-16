20bc4bc0b8a899ea02c7002a57eed4e0b7a79423
gyash3456 <gyash3456@gmail.com>
2022-07-16 23:18:36 +0530

    getAll/create/update/delete/single product completed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,4 @@
+const { findByIdAndDelete } = require("../models/productModel");
 const Product = require("../models/productModel")
 
 
@@ -35,6 +36,43 @@ exports.updateProduct =async(req,res,next)=>{
         runValidators:true,
         useFindAndModify:false
     });
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+
+//delete a product
+
+exports.deleteProduct = async(req,res,next)=>{
+    const product= await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found" 
+        })
+
+    }
+    await product.remove();
+
+    res.status(200).json({
+        success:true,
+        message:"product deleted successfully"
+    })
+}
+
+//get product details
+
+exports.getProductDetails= async(req,res,next)=>{
+    const product= await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found" 
+        })
+
+    }
     res.status(200).json({
         success:true,
         product
